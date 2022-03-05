047c27d55632301048c63112232d942dbcb44277
Vinod KC <vinod.kc.in@gmail.com>
2022-03-04 21:39:01 -0800

    Added stock API integration

--- index.mjs
+++ index.mjs
@@ -1,10 +1,22 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import path from 'path';
+import request from 'request';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// create call_api
+function call_api(finishedAPI) {
+var apiURL = 'https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_d6a5a5e42d344f83add115bc1e5f3a25';
+request(apiURL, {json:true}, (err,res, body) => {
+ if (err) { return console.log(err)}
+
+ if (res.statusCode === 200) {
+    finishedAPI(body)
+ }
+});
+}
 
 // Set hadlebars Middleware
 const hbs = engine({});
@@ -15,7 +27,17 @@ app.set('views', './views');
 const otherstuff = "This is other stuff";
 //Set handlebar route
 app.get('/', (req, res) => {
-    res.render('home',  {
+
+    call_api( (doneAPI) => {
+        res.render('home',  {
+            stock: doneAPI
+        });
+    });
+   
+});
+
+app.get('/about.html', (req, res) => {
+    res.render('about',  {
         stuff: otherstuff
     });
 });
